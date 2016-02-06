ea991345f21221a91bce16d90cbc06ca5c85781d
ThimoJonker <tammolol@hotmail.com>
2016-02-06 18:05:14 +0100

    alle opdrachten af van les 1 php.

--- school/index.php
+++ school/index.php
@@ -5,4 +5,17 @@
  * Date: 3-2-2016
  * Time: 10:49
  */
-phpinfo();
+
+
+if ($handle = opendir('.')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
